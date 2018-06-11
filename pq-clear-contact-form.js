69b9e9c2d51c05453961f5744cee9f2850523130
jsaiyapq <Jim.Saiya@proquest.com>
2018-06-11 18:48:57 -0400

    GMS-11743, 2018-04-22

--- pq-clear-contact-form.js
+++ pq-clear-contact-form.js
@@ -3,7 +3,7 @@
  * Based on Trevor G's helper-pq-contact-forms.js.
  * @title	Clear Contact Form
  * @author	Jim Saiya
- * @date	2018-02-20
+ * @date	2018-04-22
  *
  * Requires the following jQuery plug-ins to be loaded on the page:
  * - <script src="//www.proquest.com/includes/parsley.js"></script>
@@ -32,8 +32,15 @@ function initContactForm(theForm, emailField, nameField) {
 	// custom config of parsley, to remove error message
 	$(formID).parsley(parsleyOptions);
 
+	// enable email field before submitting form, otherwise value will not be sent
+	$(formID).submit(function(e) {
+		$(emailFieldID).prop('disabled', false);
+	});
+
 	// initialize the identity clearing
-	initFormIdentityLink();
+	// only perform the init if the hidden field with a recipient's ID is present
+	if ( $('[name="RECIPIENT_ID_*"]').length )
+		initFormIdentityLink();
 
 }
 
@@ -43,64 +50,67 @@ function initContactForm(theForm, emailField, nameField) {
  */
 function initFormIdentityLink() {
 
-	// only perform the init if the hidden field with a recipient's ID is present
-	if ( $('[name="RECIPIENT_ID_*"]').length ) {
+	var promptText;
+	var aTagAttributes = 'href="#" class="clear-identity-action" name="clear_the_form" xt="SPNOTRACK"';
 
-		var promptText;
-		var aTagAttributes = 'href="#" class="clear-identity-action" name="clear_the_form" xt="SPNOTRACK"';
+	// build "Clear the form" text based on form language
+	switch (formLanguage) {
 
-		// build "Clear the form" text based on form language
-		switch (formLanguage) {
+		case 'zh-Hant':	// Chinese (Traditional)
+			promptText = '<a ' + aTagAttributes + '>Clear the form</a>';
+			break;
 
-			case 'zh-hans':
-				promptText = '<a ' + aTagAttributes + '>如不是您本人，请重新填写。</a>';
-				break;
+		case 'zh-Hans':	// Chinese (Simplified)
+			promptText = '<a ' + aTagAttributes + '>如不是您本人，请重新填写。</a>';
+			break;
 
-			case 'latam-pt': 
-				promptText = 'Se você não é ' + $(nameFieldID).val() + ' <a ' + aTagAttributes + '>limpe aqui o formulário</a>.';
-				break;
+		case 'ja':		// Japanese
+			promptText = $(nameFieldID).val() + 'ではありませんか？<a ' + aTagAttributes + '>フォームをクリア</a>にしてください。';
+			break;
 
-			case 'latam-es':
-				promptText = 'Si usted no es ' + $(nameFieldID).val() + ' <a ' + aTagAttributes + '>Borre el formulario</a>.';
-				break;
+		case 'ko':		// Korean
+			promptText = $(nameFieldID).val() + '이/가 아닌가요? <a ' + aTagAttributes + '>새로 작성하기</a>.';
+			break;
 
-			default:
-				promptText = 'Not ' + $(nameFieldID).val() + '? <a ' + aTagAttributes + '>Clear the form</a>.';
-				break;
+		case 'pt-BR':	// Portuguese (Brazil)
+			promptText = 'Se você não é ' + $(nameFieldID).val() + ' <a ' + aTagAttributes + '>limpe aqui o formulário</a>.';
+			break;
 
-		}
+		case 'es-419':	// Spanish (Latin America)
+			promptText = 'Si usted no es ' + $(nameFieldID).val() + ' <a ' + aTagAttributes + '>borre el formulario</a>.';
+			break;
 
-		var promptPara = '<p id="clear-identity-prompt">' + promptText + '</p>';
-
-		// insert the "Clear the form" prompt above the form
-		$(formID).before(promptPara);
-
-		// show the email warning prompt
-		$('#email-warning').show();
-
-		// set field focus
-		$(nameFieldID).focus();
-
-		// disable email field
-		$(emailFieldID).prop('disabled', 'disabled');
-
-		// do not require validation on disabled fields
-		$(emailFieldID).removeProp('required');
-
-		// bind click event function to "Clear the form" link(s)
-		$('.clear-identity-action').click(function(e) {
-
-console.log('"Clear the form" clicked.');
-
-			// don't reload the page
-			e.preventDefault();
-
-			// reset the form
-			clearIdentity();
-
-		});
+		default:		// English
+			promptText = 'Not ' + $(nameFieldID).val() + '? <a ' + aTagAttributes + '>Clear the form</a>.';
+			break;
 
 	}
+
+	// insert the "Clear the form" prompt above the form
+	$(formID).before('<p id="clear-identity-prompt">' + promptText + '</p>');
+
+	// show the email warning prompt
+	$('#email-warning').show();
+
+	$(nameFieldID).focus();
+
+	// disable email field
+	$(emailFieldID).prop('disabled', 'disabled');
+
+	// do not require validation on disabled fields
+	$(emailFieldID).removeProp('required');
+
+	// bind click event function to "Clear the form" link(s)
+	$('.clear-identity-action').click(function(e) {
+
+		console.log('"Clear the form" clicked.'); ////////////////////
+
+		// don't reload the page
+		e.preventDefault();
+
+		clearIdentity();
+
+	});
 
 }
 
@@ -110,39 +120,29 @@ console.log('"Clear the form" clicked.');
  */
 function clearIdentity() {
 
-console.log('"clearIdentity()" called.');
+	console.log('"clearIdentity()" called.'); ////////////////////
 
-// code commented out here for testing with mock-up
-// make active for production
+	// remove the recipient id field
+	//  only to make sure it does not get sent with the new record
+	//  (not sure this is strictly required)
+	$('[name="RECIPIENT_ID_*"]').remove();
 
-	// delete the cookie
-//	var sp_identity_removed = $.removeCookie('SP_IDENTITY', { path: '/' });
+	// hide the email warning prompt
+	$('#email-warning').hide();
 
-	// if cookie removal was successful, finish the job
-//	if( sp_identity_removed ) {
+	// remove the "Clear the form" prompt from above the form
+	$('#clear-identity-prompt').html('');
 
-//console.log('Cookie removed successfully.');
+	// clear the form fields
+	$(formID).clearForm();	// (from jquery.form.js)
 
-		// remove the recipient id field
-//		$('[name="RECIPIENT_ID_*"]').remove();
+	// enable email field
+	$(emailFieldID).prop('disabled', false);
 
-		// hide the email warning prompt
-		$('#email-warning').hide();
+	// require an email address
+	$(emailFieldID).prop('required', 'required');
 
-		// remove the "Clear the form" prompt from above the form
-		$('#clear-identity-prompt').html('');
-		
-		// clear the form fields
-		$(formID).clearForm();	// (from jquery.form.js)
-
-		// enable email field
-		$(emailFieldID).prop('disabled', false);
-
-		// require an email address
-		$(emailFieldID).prop('required', 'required');
-
-		// set field focus
-		$(emailFieldID).focus();
+	$(emailFieldID).focus();
 
 //	}
 
